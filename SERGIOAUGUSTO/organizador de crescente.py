7f4364d748a192638caf41fe0fe3ab99f5d143a8
SERGIO AUGUSTO DE OLIVEIRA SOUZA <sergio.augusto@aluno.senac.rio>
2025-10-27 21:26:25 -0300

    comitado

---
+++ SERGIOAUGUSTO/organizador de crescente.py
@@ -0,0 +1,16 @@
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+c = int(input("Digite o terceiro número: "))
+
+if a <= b and b <= c:
+    print(a, b, c)
+elif a <= c and c <= b:
+    print(a, c, b)
+elif b <= a and a <= c:
+    print(b, a, c)
+elif b <= c and c <= a:
+    print(b, c, a)
+elif c <= a and a <= b:
+    print(c, a, b)
+else:
+    print(c, b, a)
